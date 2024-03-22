e3881bd9d8523000295a08bd0d263345f3aba254
us3rdie <dima.smiles.pro@gmail.com>
2024-03-22 17:35:04 +0300

    now we can save new workouts

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -1,30 +1,44 @@
+const { v4: uuid } = require('uuid');
 const Workout = require('../database/Workout');
 
 const getAllWorkouts = () => {
-  const allWorkouts = Workout.getAllWorkouts();
-  return allWorkouts;
+    const allWorkouts = Workout.getAllWorkouts();
+    return allWorkouts;
 };
 
 const getOneWorkout = () => {
-  return;
+    return;
 };
 
-const createNewWorkout = () => {
-  return;
+const createNewWorkout = (newWorkout) => {
+    const workoutToInsert = {
+        ...newWorkout,
+        id: uuid(),
+        createdAt: new Date().toLocaleString('en-US', {
+            timeZone: 'UTC',
+        }),
+        updatedAt: new Date().toLocaleString('en-US', {
+            timeZone: 'UTC',
+        }),
+    };
+    const createdWorkout = Workout.createNewWorkout(
+        workoutToInsert
+    );
+    return createdWorkout;
 };
 
 const updateOneWorkout = () => {
-  return;
+    return;
 };
 
 const deleteOneWorkout = () => {
-  return;
+    return;
 };
 
 module.exports = {
-  getAllWorkouts,
-  getOneWorkout,
-  createNewWorkout,
-  updateOneWorkout,
-  deleteOneWorkout,
+    getAllWorkouts,
+    getOneWorkout,
+    createNewWorkout,
+    updateOneWorkout,
+    deleteOneWorkout,
 };
