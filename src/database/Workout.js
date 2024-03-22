1387f14a1c4845cdbbdb7012ddf944d2aa44e2d7
us3rdie <dima.smiles.pro@gmail.com>
2024-03-22 17:48:39 +0300

    added workouts methods

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -1,9 +1,19 @@
+// In src/database/Workout.js
 const DB = require('./db.json');
-
 const { saveToDatabase } = require('./utils');
 
 const getAllWorkouts = () => {
     return DB.workouts;
+};
+
+const getOneWorkout = (workoutId) => {
+    const workout = DB.workouts.find(
+        (workout) => workout.id === workoutId
+    );
+    if (!workout) {
+        return;
+    }
+    return workout;
 };
 
 const createNewWorkout = (newWorkout) => {
@@ -19,7 +29,40 @@ const createNewWorkout = (newWorkout) => {
     return newWorkout;
 };
 
+const updateOneWorkout = (workoutId, changes) => {
+    const indexForUpdate = DB.workouts.findIndex(
+        (workout) => workout.id === workoutId
+    );
+    if (indexForUpdate === -1) {
+        return;
+    }
+    const updatedWorkout = {
+        ...DB.workouts[indexForUpdate],
+        ...changes,
+        updatedAt: new Date().toLocaleString('en-US', {
+            timeZone: 'UTC',
+        }),
+    };
+    DB.workouts[indexForUpdate] = updatedWorkout;
+    saveToDatabase(DB);
+    return updatedWorkout;
+};
+
+const deleteOneWorkout = (workoutId) => {
+    const indexForDeletion = DB.workouts.findIndex(
+        (workout) => workout.id === workoutId
+    );
+    if (indexForDeletion === -1) {
+        return;
+    }
+    DB.workouts.splice(indexForDeletion, 1);
+    saveToDatabase(DB);
+};
+
 module.exports = {
     getAllWorkouts,
     createNewWorkout,
+    getOneWorkout,
+    updateOneWorkout,
+    deleteOneWorkout,
 };
