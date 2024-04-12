d85f0efd3c4894b58a7380126912de2561eafd9f
us3rdie <dima.smiles.pro@gmail.com>
2024-04-12 20:41:05 +0300

    added swagger ui

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -4,6 +4,12 @@
  *   get:
  *     tags:
  *       - Workouts
+ *     parameters:
+ *       - in: query
+ *         name: mode
+ *         schema:
+ *           type: string
+ *         description: The mode of a workout
  *     responses:
  *       200:
  *         description: OK
@@ -19,8 +25,23 @@
  *                   type: array
  *                   items:
  *                     $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Some error message"
  */
-
 
 const express = require('express');
 const workoutController = require('../../controllers/workoutController');
